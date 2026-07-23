7864bd3cf1fe9afb11792c68bc47cdbb0b26afe8
Tan Phung <tanphung000@gmail.com>
2026-07-23 21:13:29 +0700

    deploy: point production config to final Bradbury contract

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.CLAUSEFLOW_CONFIG = {
-  contractAddress: "0x634C7628b593C41C26F4Cf19E4fF919310696b22",
+  contractAddress: "0x0Ed7171962F2694a15Ba81114FDC08f970B5B6F4",
   chain: "testnetBradbury",
   explorerUrl: "https://explorer-bradbury.genlayer.com",
   stateStatus: "accepted"
